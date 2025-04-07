ec422428f5105a3387c8a19d6c6b13552a419f69
ShubhamMulik2409 <srmulik2409@gmail.com>
2025-04-08 01:42:57 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "batch-27-shubham-mulik-bucket-01-sample"
+    bucket = "batch-27-shubham-mulik-bucket-01-sample-02"
     key = "LondonKey_1"
     region = "eu-west-2"
     dynamodb_table = "my-dynamodb-table"
